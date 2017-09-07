ab1693dc2f0f909807d510e7d0465dd7c5d270c1
st-brooklyn <dhanakrit.s@gmail.com>
2017-09-07 12:22:00 +0700

    test

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -235,9 +235,9 @@ function handleEvent(event) {
 
                 var api_request = require('request');
                 api_request.get({
-                    url: 'http://apiwowtest.softsq.com/jsonSOA/getdata.ashx?APIKey=APImushroomtravel&mode=searchresultsproduct&country_slug=japan',
-                    json: true,
-                    headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+                    url: 'http://192.168.20.149:9001/jsonSOA/getdata.ashx?APIKey=APImushroomtravel&mode=searchresultsproduct&country_slug=japan',
+                    json: true
+                    //headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
                 }, (apierr, apiresponse, apidata) => {
                     if(apierr) return handleError("[API Mockup] " + apierr.stack, "ERROR");
                     handleError("[API Mockup]" + JSON.stringify(apidata), "DEBUG");                    

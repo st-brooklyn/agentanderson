8a0533272c0028243906c46c4c3566b845c604b8
st-brooklyn <dhanakrit.s@gmail.com>
2017-09-12 17:18:43 +0700

    test

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -48,7 +48,7 @@ function createProductCarousel(products) {
             "thumbnailImageUrl": product.url_pic.replace("http","https"),
             "title": product.product_name.substr(0, 40),
             "text": product.periods.forEach((period) =>{
-                    period.period_start
+                     "text" = period.period_start;
                 }) ,
             "actions": [                
                 {

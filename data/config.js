d1cb004f19f0580bfa07658de5779ee6f800353b
st-brooklyn <dhanakrit.s@gmail.com>
2017-09-14 17:34:24 +0700

    test

--- data/config.js
+++ data/config.js
@@ -2,5 +2,5 @@ module.exports = {
     lineChannelAccessToken: 'dIZf/b/ZabUO0IafFmPxBvcG9xPKQXtGZ6wClV70CCqTwV1TJDT1m58rdm3pko08nIimFRk5wmcElbc7mF9ZXkntG7goq5NDifdSJBkGLyReznHswZuhR77uOYc9ryJIVAfhouccWFwtKMIMucBXpQdB04t89/1O/w1cDnyilFU=',
     lineChannelSecret: '912ad53b5e85ed684a9c52ac621d77e9',
     apiUrl: 'http://apitest.softsq.com:9001/JsonSOA/getdata.ashx',
-    recastRequestToken: 'a77e7abf77a2bb08422e96d0d1a7a84c'
+    recastRequestToken: '1307df439794fa6122aa0f939f7d4c58'
 }

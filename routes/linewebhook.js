16831418c7aa3fb553e14a11d0bb2eb8ef1105eb
st-brooklyn <dhanakrit.s@gmail.com>
2017-09-14 14:11:20 +0700

    test

--- routes/linewebhook.js
+++ routes/linewebhook.js
@@ -229,12 +229,12 @@ function handleEvent(event) {
                 // Call api tour    
                 var entities = recast_response.entities;
                 handleError("[Main] entities?: " + JSON.stringify(entities), "INFO");
-                var country = entities['country'] == undefined ? null : entities['country'].value
-                var tourcode = entities['tourcode'] == undefined ? null : entities['tourcode'].value
+                var country = entities.country == undefined ? null : entities.country.value
+                var tourcode = entities.tourcode == undefined ? null : entities.tourcode.value
                 var departuredate = entities['departure-date'] == undefined ? null : entities['departure-date'].value
-                var returndate = entities['returndate'] == undefined ? null : entities['returndate'].value
-                var month = entities['month'] == undefined ? null : entities['month'].value
-                var traveler = entities['traveler'] == undefined ? null : entities['traveler'].value
+                var returndate = entities.returndate == undefined ? null : entities.returndate.value
+                var month = entities.month == undefined ? null : entities.month.value
+                var traveler = entities.traveler == undefined ? null : entities.traveler.value
                 // Call function convert country to country_slug 
                 // Call function convert city to city_slug 
                 // Call function convert airline name to airline code
@@ -243,16 +243,16 @@ function handleEvent(event) {
                 
                 // Construct the reply message
                 handleError("[API] Before Param: country = " + country + " tourcode = " + tourcode + " departuredate = " + departuredate + " returndate = " + returndate + " month = " + month + " traveler = " + traveler, "DEBUG");
-                if (country && departuredate && returndate && month)
-                {
-                    handleError("[API] check param: country = " + country + " tourcode = " + tourcode + " departuredate = " + departuredate + " returndate = " + returndate + " month = " + month + " traveler = " + traveler, "DEBUG");
-                }
+              
                 // tourresuilt = tour.gettour(cpuntry, city, periond, pax)
                 var mockup_products = null
-                
-                const ta = require('../controllers/tourapicontroller');
-                mockup_products = ta.searchtour;
 
+                if (country && departuredate && returndate && month)
+                {       
+                    const ta = require('../controllers/tourapicontroller');
+                    mockup_products = ta.searchtour;
+                    handleError("[API] check param: country = " + country + " tourcode = " + tourcode + " departuredate = " + departuredate + " returndate = " + returndate + " month = " + month + " traveler = " + traveler, "DEBUG");
+                }
                 // var rpoptions = {
                 //         uri: 'http://apitest.softsq.com:9001/JsonSOA/getdata.ashx',
                 //         qs: {

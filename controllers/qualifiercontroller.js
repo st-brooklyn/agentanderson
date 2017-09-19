81f3c66874b1b9a5323d672e7aff9867ce9570ab
Brooklyn Steinfeld <reasontoalive@hotmail.com>
2017-09-19 16:48:53 +0700

    test

--- controllers/qualifiercontroller.js
+++ controllers/qualifiercontroller.js
@@ -1,6 +1,7 @@
 const logger = require('./logcontroller');
 const Mapping = require('../models/mapping');
 
+
 exports.qualify_get = function(id){
     logger.debug("[Qualify]", {mappingId: id});
 
@@ -12,6 +13,7 @@ exports.qualify_get = function(id){
         var reply = foundone.replyMessage;
 
         // send message to room
+        var lineclient = new require('@line/bot-sdk').Client(config);
         lineclient.pushMessage(roomId, JSON.parse(reply))
         Mapping.findByIdAndUpdate(id, 
             {$set: {action: "YES"}}, 

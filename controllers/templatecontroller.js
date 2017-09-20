61c812f4af14ac8604d8d7f6a0085e8015c0c356
st-brooklyn <dhanakrit.s@gmail.com>
2017-09-20 18:28:27 +0700

    seat

--- controllers/templatecontroller.js
+++ controllers/templatecontroller.js
@@ -25,8 +25,8 @@ module.exports.templateCarousel = function(products){
         }
         var column = {
             "thumbnailImageUrl": product.url_pic.startsWith('https', 0) ? product.url_pic : product.url_pic.replace("http","https"),
-            "title": product.product_name.substr(0, 40),
-            "text":  periodText.substr(0, 50) + '\n' + '[[seat]]',
+            "title": '[[seat]]' + product.product_name.substr(0, 40),
+            "text": periodText.substr(0, 50),
             "actions": [                
                 {
                     "type": "uri",
